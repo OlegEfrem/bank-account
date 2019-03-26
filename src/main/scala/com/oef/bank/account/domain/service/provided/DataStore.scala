f1b76961f6f1651a55d3c360e624fd33e8133b56
Oleg Efrem <oleg.efrem@yahoo.com>
2019-03-26 22:34:10 +0000

    correct DataStore scala docs to fix heroku build error

--- src/main/scala/com/oef/bank/account/domain/service/provided/DataStore.scala
+++ src/main/scala/com/oef/bank/account/domain/service/provided/DataStore.scala
@@ -36,21 +36,21 @@ trait DataStore {
     * */
   def add(transaction: Transaction, to: AccountId): Future[NewBalance]
 
-  /** Adds a new transaction to an existing account.
-    * @param by account for which to return the transactions.
-    * @return Future completed with:
-    *         - transactions for the requested account;
-    *         - AccountNotFoundException if account not found.
-    * */
   /** Rollbacks a transaction when needed (in case of a failure for example).
     * @param transaction transaction to be rollback-ed.
-    * @on which account transactions belongs to.
-    * @return Fututure completed with:
+    * @param from which account transactions belongs to.
+    * @return Future completed with:
     *         - Success in case of a successful rollback;
     *         - Failure in case of an unsuccessful rollback or a non existing account.
     * */
   def remove(transaction: Transaction, from: AccountId): Future[Unit]
 
+  /** Reads transactions of the given account.
+    * @param by account for which to read transactions.
+    * @return Future completes with:
+    *         - list of transactions with required account;
+    *         - AccountNotFoundException if account not found.
+    * */
   def readTransactions(by: AccountId): Future[List[Transaction]]
 
   /** Retrieves balance for given account id.
